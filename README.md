beb639a23a331c7765143159acacbdd427caaed5
21stChrom <81393334+21stChrom@users.noreply.github.com>
2023-07-01 12:03:00 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 21stChrom-Rate
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-4sniyq)
---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [timer, setTimer] = useState(0);
+    const [counter, setCounter] = useState(0);
+      const [profileName, setProfileName] = useState("");
+        const [employeeList, setEmployeeList] = useState([]);
+
+          const handleStart = () => {
+              setTimer(0);
+                  const interval = setInterval(() => {
+                        setTimer(timer + 1);
+                            }, 1000);
+
+                                return () => {
+                                      clearInterval(interval);
+                                          };
+                                            };
+
+                                              const handleStop = () => {
+                                                  clearInterval(timerInterval);
+                                                    };
+
+                                                      const handleReset = () => {
+                                                          setTimer(0);
+                                                              setCounter(0);
+                                                                };
+
+                                                                  const handleIncrement = () => {
+                                                                      setCounter(counter + 1);
+                                                                        };
+
+                                                                          const handleDecrement = () => {
+                                                                              setCounter(counter - 1);
+                                                                                };
+
+                                                                                  const handleSelectEmployee = (employeeName) => {
+                                                                                      setProfileName(employeeName);
+                                                                                        };
+
+                                                                                          const handleSubmit = () => {
+                                                                                              const data = {
+                                                                                                    timer: timer,
+                                                                                                          counter: counter,
+                                                                                                                profileName: profileName,
+                                                                                                                    };
+
+                                                                                                                        fetch("http://localhost:3000/api/submit", {
+                                                                                                                              method: "POST",
+                                                                                                                                    body: JSON.stringify(data),
+                                                                                                                                        });
+                                                                                                                                          };
+
+                                                                                                                                            return (
+                                                                                                                                                <div className="App">
+                                                                                                                                                      <h1>Timer</h1>
+                                                                                                                                                            <div>
+                                                                                                                                                                    <p>Timer: {timer}</p>
+                                                                                                                                                                            <button onClick={handleStart}>Start</button>
+                                                                                                                                                                                    <button onClick={handleStop}>Stop</button>
+                                                                                                                                                                                            <button onClick={handleReset}>Reset</button>
+                                                                                                                                                                                                  </div>
+                                                                                                                                                                                                        <div>
+                                                                                                                                                                                                                <p>Counter: {counter}</p>
+                                                                                                                                                                                                                        <button onClick={handleIncrement}>+</button>
+                                                                                                                                                                                                                                <button onClick={handleDecrement}>-</button>
+                                                                                                                                                                                                                                        <button onClick={handleReset}>Reset</button>
+                                                                                                                                                                                                                                              </div>
+                                                                                                                                                                                                                                                    <div>
+                                                                                                                                                                                                                                                            <p>Profile Name: {profileName}</p>
+                                                                                                                                                                                                                                                                    <select
+                                                                                                                                                                                                                                                                              name="employeeName"
+                                                                                                                                                                                                                                                                                        onChange={(event) => setProfileName(event.target.value)}
+                                                                                                                                                                                                                                                                                                >
+                                                                                                                                                                                                                                                                                                          {employeeList.map((employee) => (
+                                                                                                                                                                                                                                                                                                                      <option key={employee} value={employee}>
+                                                                                                                                                                                                                                                                                                                                    {employee}
+                                                                                                                                                                                                                                                                                                                                                </option>
+                                                                                                                                                                                                                                                                                                                                                          ))}
+                                                                                                                                                                                                                                                                                                                                                                  </select>
+                                                                                                                                                                                                                                                                                                                                                                        </div>
+                                                                                                                                                                                                                                                                                                                                                                              <button onClick={handleSubmit}>Submit</button>
+                                                                                                                                                                                                                                                                                                                                                                                  </div>
+                                                                                                                                                                                                                                                                                                                                                                                    );
+                                                                                                                                                                                                                                                                                                                                                                                    }
+
+                                                                                                                                                                                                                                                                                                                                                                                    export default App;
+                                                                                                                                                                                                                                                                                                                                                                                    
